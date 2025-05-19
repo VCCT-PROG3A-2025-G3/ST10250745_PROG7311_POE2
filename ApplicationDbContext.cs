61b972e78dc9f45a14006a7581683cb124fa9dbf
ST10250745 <ST10250745@vcconnect.edu.za>
2025-05-19 17:50:24 +0200

    Data

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using PROG7311POE2.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PROG7311POE2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Farmer> Farmers { get; set; }
+    }
+}
